e3489365a15cf3e9176050726d1657912384b7eb
DirtyHarryE <harridunc@gmail.com>
2016-09-19 11:27:30 +0100

    Added PList modification

---
+++ Assets/Editor/ChangeIOSEncryptionPList.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+ 
+public class ChangeIOSEncryptionPList
+{
+    [PostProcessBuild]
+    public static void ChangeEncryptionPList(BuildTarget buildTarget, string pathToBuiltProject)
+    {
+        if (buildTarget == BuildTarget.iOS)
+        {
+            // Get plist
+            string plistPath = pathToBuiltProject + "/Info.plist";
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
+
+            // Get root
+            PlistElementDict rootDict = plist.root;
+
+            // Change value of CFBundleVersion in Xcode plist
+            var buildKey = "ITSAppUsesNonExemptEncryption ";
+            rootDict.SetString(buildKey, "true");
+
+            // Write to file
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
+    }
+}
