2e0d23ac48390ad020656f44c0bf9ba31e098ca1
DirtyHarryE <harridunc@gmail.com>
2016-09-19 22:16:48 +0100

    New build increment settings

---
+++ Assets/Editor/CommonUtils.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class CommonUtils
+{
+    public static string ReadTextFile(string sFileName)
+    {
+        //Debug.Log("Reading " + sFileName);
+
+        //Check to see if the filename specified exists, if not try adding '.txt', otherwise fail
+        string sFileNameFound = "";
+        if (File.Exists(sFileName))
+        {
+            //Debug.Log("Reading '" + sFileName + "'.");
+            sFileNameFound = sFileName; //file found
+        }
+        else if (File.Exists(sFileName + ".txt"))
+        {
+            sFileNameFound = sFileName + ".txt";
+        }
+        else
+        {
+            //Debug.Log("Could not find file '" + sFileName + "'.");
+            return null;
+        }
+
+        StreamReader sr;
+        try
+        {
+            sr = new StreamReader(sFileNameFound);
+        }
+        catch (System.Exception e)
+        {
+            //Debug.LogWarning("Something went wrong with read.  " + e.Message);
+            return null;
+        }
+
+        string fileContents = sr.ReadToEnd();
+        sr.Close();
+
+        return fileContents;
+    }
+
+    public static void WriteTextFile(string sFilePathAndName, string sTextContents)
+    {
+        if (!File.Exists(sFilePathAndName))
+        {
+
+            string[] dirs = sFilePathAndName.Split('/');
+            string str = "";
+            for (int i = 0; i < dirs.Length - 1; i++)
+            {
+                str = i == 0 ? dirs[0] : str + "/" + dirs[i];
+                if (!Directory.Exists(str))
+                {
+                    Directory.CreateDirectory(str);
+                }
+            }
+        }
+        StreamWriter sw = new StreamWriter(sFilePathAndName);
+        
+        sw.WriteLine(sTextContents);
+        sw.Flush();
+        sw.Close();
+    }
+}
---
+++ Assets/Editor/IncrementBuildNo.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using System;
+
+public class IncrementBuildNo
+{
+
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        ReadVersionAndIncrement(pathToBuiltProject, true);
+    }
+
+    [MenuItem("Terahard/Increment Build Version")]
+    private static void Increment()
+    {
+        ReadVersionAndIncrement();
+    }
+    [MenuItem("Terahard/Increment and Assign Build Version")]
+    private static void IncrementAndAssign()
+    {
+        ReadVersionAndIncrement(true);
+    }
+    private static void ReadVersionAndIncrement(bool assign)
+    {
+        ReadVersionAndIncrement(null, assign);
+    }
+    private static void ReadVersionAndIncrement(string buildPath = null, bool assign = false)
+    {
+        //the file name and path.  No path is the base of the Unity project directory (same level as Assets folder)
+        string buildTextFileNameAndPath = Application.streamingAssetsPath + "/build.txt";
+
+        int buildInt = 0;
+        string buildText = CommonUtils.ReadTextFile(buildTextFileNameAndPath);
+        bool success = int.TryParse(buildText, out buildInt);
+
+        if (success && buildText != null)
+        {
+            buildText = buildText.Trim(); //clean up whitespace if necessary
+            int i = int.Parse(buildText);
+            i += 1;
+            buildInt = i;
+            Debug.Log("Build Incremented " + buildInt);
+
+            //save the file (overwrite the original) with the new version number
+            CommonUtils.WriteTextFile(buildTextFileNameAndPath, buildInt.ToString());
+
+            //tell unity the file changed (important if the versionTextFileNameAndPath is in the Assets folder)
+            AssetDatabase.Refresh();
+        }
+        else
+        {
+            buildInt = 0;
+            //no file at that path, make it
+            CommonUtils.WriteTextFile(buildTextFileNameAndPath, buildInt.ToString());
+        }
+
+        if (buildPath != null)
+        {
+            string[] parts = buildPath.Split('\\');
+            string finalBuildPath = "";
+            for (int i = 0; i < parts.Length - 2; i++)
+            {
+                finalBuildPath += parts[i];
+            }
+            finalBuildPath += parts[parts.Length - 1].Replace(".exe", "_Data/") + "StreamingAssets/version.txt";
+            Debug.Log(finalBuildPath);
+            CommonUtils.WriteTextFile(finalBuildPath, buildInt.ToString());
+        }
+
+        if (assign)
+        {
+            PlayerSettings.Android.bundleVersionCode = buildInt;
+            PlayerSettings.iOS.buildNumber = buildInt.ToString();
+        }
+    }
+}
