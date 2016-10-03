cf5286a044c9f8e9c45900a43844df244181f495
Harry Evans <harry.evans@happyfinish.com>
2016-10-03 18:25:47 +0100

    Added build number increase to build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -44,6 +44,7 @@ public class BuildScript
     [MenuItem("Custom/CI/Build Android")]
     private static void PerformAndroidBuild()
     {
+        PlayerSettings.Android.bundleVersionCode = GetBuildNumber();
         PlayerSettings.bundleIdentifier = "com.Terahard.Teracasino";
         string targetDir = APP_NAME;
         GenericBuild(SCENES, TARGET_DIR + "/Android/" + targetDir, BuildTarget.Android, BuildOptions.None);
@@ -52,8 +53,14 @@ public class BuildScript
     [MenuItem("Custom/CI/Build iOS")]
     private static void PerformIosBuild()
     {
+        PlayerSettings.iOS.buildNumber = GetBuildNumber().ToString();
         string targetDir = APP_NAME;
         GenericBuild(SCENES, TARGET_DIR + "/Ios/" + targetDir, BuildTarget.iOS, BuildOptions.None);
+    }
+    [MenuItem("Terahard/IncrementBuildNumber")]
+    private static void IncrementBuildNumberMenu()
+    {
+        IncrementBuildNumber();
     }
 
     private static string[] FindEnabledEditorScenes()
@@ -83,8 +90,82 @@ public class BuildScript
         }
     }
 
+    private static string RootFolder
+    {
+        get
+        {
+            string path = Application.dataPath;
+            string[] components = path.Split('/');
+            string newPath = "";
+            for (int i = 0; i < components.Length -1; i++)
+            {
+                newPath = string.IsNullOrEmpty(newPath) ? components[i] : newPath + "/" + components[i];
+            }
+            return newPath;
+        }
+    }
 
+    private static string BuildFileLocation
+    {
+        get
+        {
+            return RootFolder + "/fastlane/build.txt";
+        }
+    }
+    private static string VersionFileLocation
+    {
+        get
+        {
+            return RootFolder + "/fastlane/version.txt";
+        }
+    }
+    private static void IncrementBuildNumber()
+    {
+        string versionFileLocation = VersionFileLocation;
+        string buildFileLocation = BuildFileLocation;
 
+        bool makeNewBuildFile = false;
+        string currentVersion = PlayerSettings.bundleVersion.Trim();
+        string loadedVersion = CommonUtils.ReadTextFile(versionFileLocation).Trim();
+        if (!currentVersion.Equals(loadedVersion))
+        {
+            CommonUtils.WriteTextFile(versionFileLocation, currentVersion);
+            makeNewBuildFile = true;
+        }
+        Debug.Log("Version : " + currentVersion + ", " + loadedVersion + " = " + currentVersion.Equals(loadedVersion));
+
+        
+        int buildInt = 0;
+        string buildText = CommonUtils.ReadTextFile(buildFileLocation);
+        bool success = int.TryParse(buildText, out buildInt);
+
+        if (success && buildText != null && !makeNewBuildFile)
+        {
+            buildText = buildText.Trim(); //clean up whitespace if necessary
+            int i = int.Parse(buildText);
+            i += 1;
+            buildInt = i;
+            Debug.Log("\nBuild Incremented : " + buildInt);
+
+            //save the file (overwrite the original) with the new version number
+            CommonUtils.WriteTextFile(buildFileLocation, buildInt.ToString());
+
+            //tell unity the file changed (important if the versionTextFileNameAndPath is in the Assets folder)
+            AssetDatabase.Refresh();
+        }
+        else
+        {
+            buildInt = 0;
+            //no file at that path, make it
+            CommonUtils.WriteTextFile(buildFileLocation, buildInt.ToString());
+        }
+    }
+    private static int GetBuildNumber( )
+    {
+        string buildTextFileNameAndPath = BuildFileLocation;
+        string buildText = CommonUtils.ReadTextFile(buildTextFileNameAndPath);
+        return int.Parse(buildText);
+    }
 
 
 
@@ -95,7 +176,6 @@ public class BuildScript
 
     public static void ChangeEncryptionPList(BuildTarget build_target, string targetDir)
     {
-        Debug.Log("+++++++++++++++++++++++++++++++++++++++++++++++++++++\nTesting PostProcessBuild [" + build_target.ToString() + "]\n++++++++++++++++++++++++++++++++++++++++++++++++++++");
         if (build_target == BuildTarget.iOS)
         {
             // Get plist
